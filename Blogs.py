676849b338a5918cc860d6ea9eac7efae2758ae2
sneha <snehamainkar11@gmail.com>
2020-08-20 22:55:05 +0530

    first commit

---
+++ Blogs.py
@@ -0,0 +1,7 @@
+from Blogs import  create_app
+
+app = create_app()
+if __name__ == '__main_run.py_':
+    app.run(debug=True)
+
+#Blogs.py runserver
---
+++ Blogs/__init__.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_mail import Mail
+from Blogs.config import Config
+
+
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
+mail = Mail()
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from Blogs.users.routes import users
+    from Blogs.posts.routes import posts
+    from Blogs.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
